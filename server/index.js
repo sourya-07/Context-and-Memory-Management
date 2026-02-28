ea2fbe2231203f427fd3574a02a052da9b3f579f
Sourya <souryagupta2005@gmail.com>
2026-02-28 23:09:06 +0530

    Allow CORS from any origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5080
 
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 
 app.get("/", (req, res) => {
