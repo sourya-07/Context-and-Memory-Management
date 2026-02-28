5f46f76d600f616ab1c7006159dd817ba018fd0c
Sourya <souryagupta2005@gmail.com>
2026-02-28 23:03:10 +0530

    allowed external host

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "context-and-memory-management-production-ready.onrender.com"
+    ]
+  }
 })
